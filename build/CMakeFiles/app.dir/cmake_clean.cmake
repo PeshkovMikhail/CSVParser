cf5742b1771ce0013d9c665af9fba3179c548338
Mikhail Peshkov <mikhailpeshkov03@gmail.com>
2022-12-04 22:12:23 +0700

    screen fix

---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/csv_parser.cpp.o"
+  "CMakeFiles/app.dir/csv_parser.cpp.o.d"
+  "CMakeFiles/app.dir/main.cpp.o"
+  "CMakeFiles/app.dir/main.cpp.o.d"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
